f3f6a868e4eebe6fabebccf294c4de96fc08c843
lkrbalci <lkrbalci@gmail.com>
2020-08-16 20:42:48 +0300

    before webpack

--- src/index.js
+++ src/index.js
@@ -1,22 +1,148 @@
 class toDo {
-    constructor (what, when, description, done) {
+    constructor (what, project, when, description, done) {
         this.what = what;
+        this.project = project;
         this.when = when;
         this.description = description;
         this.done = done;
     }
 }
 
+Storage.prototype.setObj = function(key, obj) {
+    return this.setItem(key, JSON.stringify(obj))
+}
+Storage.prototype.getObj = function(key) {
+    return JSON.parse(this.getItem(key))
+}
+
 toDoArray = [];
+projectArray = [];
 
-eat = new toDo("eat", "3 times a day", "healthy", "false");
-toDoArray.push(eat);
+if (localStorage.getObj("toDoArrayStorage") != null) {
+    toDoArray = localStorage.getObj("toDoArrayStorage");
+} else {
+    eat = new toDo("Eat", "Daily", "At least 3 times a day", "Eat healthy", "false");
+    toDoArray.push(eat);
+    sleep = new toDo("Sleep", "Daily", "Default todos. can be removed.", "Long and deep", "false");
+    toDoArray.push(sleep);
+    localStorage.setObj("toDoArrayStorage", toDoArray);
+}
 
-sleep = new toDo("sleep", "every night", "long", "false");
-toDoArray.push(sleep);
+if (localStorage.getObj("projectArrayStorage") == null) {
+    projectArray.push("Daily");
+    localStorage.setObj("projectArrayStorage", projectArray);
+} else {
+    projectArray = localStorage.getObj("projectArrayStorage");
+}
 
-sex = new toDo("sex", "3 a week", "safe", "true");
-toDoArray.push(sex);
+
+localStorage.setObj("toDoArrayStorage", toDoArray);
+
+localStorage.setObj("projectArrayStorage", projectArray);
+
+const listProjects = () => {
+    const leftDiv = document.getElementById("projectsContainer");
+    const table = document.createElement("table");
+    table.id = "projectsTable";
+    projectArray.forEach(element => {
+        const row = table.insertRow();
+        const cell = row.insertCell();
+        cell.innerHTML = element;
+        const cellDel = row.insertCell();
+        const addDeletePic = document.createElement("img");
+        addDeletePic.id = projectArray.indexOf(element);
+        addDeletePic.src = "../src/deleteSmall.png"
+        addDeletePic.addEventListener("click", function() {deleteProject(addDeletePic.getAttribute("id"))});
+        cellDel.appendChild(addDeletePic);
+        cellDel.style.textAlign="center";
+    })
+    const header = table.createTHead();
+    const row = header.insertRow();
+    const cell = row.insertCell();
+    cell.innerHTML = "Projects";
+    leftDiv.appendChild(table);
+    
+}
+listProjects();
+
+saveProject = () => {
+    let name = document.getElementById("addProjectFormName").value;
+    projectArray.push(name);
+    document.getElementById("projectsContainer").innerHTML = "";
+    listProjects();
+    localStorage.setObj("projectArrayStorage", projectArray);
+}
+
+cancelProject = () => {
+    document.getElementById("addProjectFormName").value = "";
+    document.getElementById("addProjectForm").style.display = "none";
+}
+
+deleteProject = (id) => {
+    projectArray.splice(id, 1);
+    document.getElementById("projectsContainer").innerHTML = ""
+    listProjects();
+    localStorage.setObj("projectArrayStorage", projectArray);
+}
+
+const listToDos = () => {
+    const container = document.getElementById("container");
+    let iterator = 0;
+    const table = document.createElement("table");
+    table.id = "toDotable"
+    toDoArray.forEach(element => {
+        const row = table.insertRow();
+        const cell = row.insertCell();
+        const addDeletePic = document.createElement("img");
+        addDeletePic.src = "../src/deleteSmall.png";
+        addDeletePic.id = iterator
+        addDeletePic.addEventListener("click", function() { deleteToDo(addDeletePic.getAttribute("id"))});
+        cell.style.textAlign = "center";
+        cell.appendChild(addDeletePic);
+        for (prop in element) {
+            if (prop == "what") {
+                const cell = row.insertCell();
+                cell.innerHTML = element[prop];
+                cell.id = iterator;
+                cell.addEventListener("click", function() {updateToDoForm(cell.getAttribute("id"))});
+                cell.style.cursor = "pointer";
+                cell.style.textDecoration = "underlined"
+            } else if (prop != "done") {     
+                const cell = row.insertCell();
+                cell.innerHTML = element[prop];
+            } else {
+                const cell = row.insertCell();
+                const addDonePic = document.createElement("img");
+                addDonePic.id = iterator;
+                addDonePic.addEventListener("click", function() {changeDone(addDonePic.getAttribute("id"))})
+                if (element[prop] == "true") {
+                    addDonePic.src = "../src/yesSmall.png";
+                } else {
+                    addDonePic.src = "../src/noSmall.png";
+                }
+                cell.appendChild(addDonePic);
+            }
+        }
+        iterator +=1;
+    })
+    const header = table.createTHead();
+    header.id = "header"
+    const row = header.insertRow();
+    let cell = row.insertCell();
+    cell.innerHTML = "Delete";
+    cell.style.width = "80px";
+    cell = row.insertCell();
+    cell.innerHTML = "What";
+    cell = row.insertCell();
+    cell.innerHTML = "Project";
+    cell = row.insertCell();
+    cell.innerHTML = "When";
+    cell = row.insertCell();
+    cell.innerHTML = "Description";
+    cell = row.insertCell();
+    cell.innerHTML = "Done";
+    container.appendChild(table);
+}
 
 const loadToDos = () => {
     const container = document.getElementById("container");
@@ -43,6 +169,8 @@ const loadToDos = () => {
                 else {
                     addDonePic.src = "../src/no.png";
                 }
+                addDonePic.id = iterator;
+                addDonePic.addEventListener("click", function() {changeDone(addDonePic.getAttribute("id"))});
                 divToDo.appendChild(addDonePic);
             }
         }
@@ -56,16 +184,127 @@ const loadToDos = () => {
     });
 }
 
+renderToDos = () => {
+    container.innerHTML = "";
+    if (document.getElementById("styleSelector").textContent == "Show Cards") {
+        listToDos();
+    } else {
+        loadToDos();
+    }
+}
+
+renderToDos();
+
 deleteToDo = (id) => {
-    console.log(id);
     toDoArray.splice(id,1);
     const container = document.getElementById(`container`);
-    container.innerHTML = "";
-    loadToDos();
+    renderToDos();
+    localStorage.setObj("toDoArrayStorage", toDoArray);
 }
 
-addForm = () => {
-    // display a form to add a toDo. blackout background
+addToDoForm = () => {
+    document.getElementById("addProjectForm").style.display = "none";
+    document.getElementById("toDoCardForm").style.display = "none";
+    const projectSelect = document.getElementById("projectSelect");
+    projectSelect.innerHTML = "";
+    for (let i = 0; i < projectArray.length; i++) {
+        let project = projectArray[i];
+        let option = document.createElement("option");
+        option.textContent = project;
+        option.value = project;
+        projectSelect.appendChild(option);
+    }
+    document.getElementById("addToDoForm").style.display = "block";
+}
+addProjectForm = () => {
+    document.getElementById("addToDoForm").style.display = "none";
+    document.getElementById("toDoCardForm").style.display = "none";
+    document.getElementById("addProjectForm").style.display = "block";
 }
 
-loadToDos();
+saveToDo = () => {
+    let what = document.getElementById("addToDoFormWhat").value;
+    let project = document.getElementById("projectSelect").value;
+    let when = document.getElementById("addToDoFormWhen").value;
+    let description = document.getElementById("addToDoFormDescription").value;
+    let done = ""
+    if (document.getElementById("addToDoFormDone").checked) {
+        done = "true";
+    } else {
+        done = "false";
+    }
+    let newToDo = new toDo(what, project, when, description, done);
+    toDoArray.push(newToDo);
+    renderToDos();
+    localStorage.setObj("toDoArrayStorage", toDoArray);
+    cancelToDo();
+}
+
+updateToDoForm = (id) => {
+    document.getElementById("addToDoForm").style.display = "none";
+    document.getElementById("addProjectForm").style.display = "none";
+    const toDoCardFormProject = document.getElementById("toDoCardFormProject");
+    toDoCardFormProject.innerHTML = "";
+    for (let i = 0; i < projectArray.length; i++) {
+        let project = projectArray[i];
+        let option = document.createElement("option");
+        option.textContent = project;
+        option.value = project;
+        toDoCardFormProject.appendChild(option);
+    }
+    document.getElementById("toDoCardForm").style.display = "block";
+    document.getElementById("toDoCardFormWhat").value = toDoArray[id].what;
+    document.getElementById("toDoCardFormWhen").value = toDoArray[id].when;
+    document.getElementById("toDoCardDescription").value = toDoArray[id].description;
+    if (toDoArray[id].done == "true") {
+        document.getElementById("toDoCardDone").checked = true;
+    } else {
+        document.getElementById("toDoCardDone").checked = false;
+
+    }
+    document.getElementById("updateButton").addEventListener("click", fn = function(){updateToDo(id)});    
+}
+
+updateToDo = (id) => {
+    toDoArray[id].what = document.getElementById("toDoCardFormWhat").value;
+    toDoArray[id].when = document.getElementById("toDoCardFormWhen").value;
+    toDoArray[id].project = document.getElementById("toDoCardFormProject").value;
+    toDoArray[id].description = document.getElementById("toDoCardDescription").value;
+    if (document.getElementById("toDoCardDone").checked) {
+        toDoArray[id].done = "true";
+    } else {
+        toDoArray[id].done = "false";
+    }
+    document.getElementById("updateButton").removeEventListener("click", fn);
+    cancelToDo();
+    renderToDos();
+    localStorage.setObj("toDoArrayStorage", toDoArray);
+}
+
+cancelToDo = () => {
+    document.getElementById("addToDoFormWhat").value = "";
+    document.getElementById("addToDoFormWhen").value = "";
+    document.getElementById("addToDoFormDescription").value = "";
+    document.getElementById("addToDoFormDone").checked = "false";
+    document.getElementById("addToDoForm").style.display = "none";
+    document.getElementById("toDoCardForm").style.display = "none";
+}
+
+changeToDoRender = () => {
+    if (document.getElementById("styleSelector").textContent == "Show List") {
+        document.getElementById("styleSelector").textContent = "Show Cards";
+    } else {
+        document.getElementById("styleSelector").textContent = "Show List";
+    }
+    renderToDos();
+}
+
+changeDone = (id) => {
+    if (toDoArray[id].done == "true") {
+        toDoArray[id].done = "false";
+    } else {
+        toDoArray[id].done = "true";
+    }
+    renderToDos();
+    localStorage.setObj("toDoArrayStorage", toDoArray);
+}
